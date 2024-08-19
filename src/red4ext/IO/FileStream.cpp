28c4bec786dc58fe463bfc48d76e3357518ef435
tapiocadao <97900530+tapiocadao@users.noreply.github.com>
2024-08-19 17:46:52 -0400

    Update FileStream.cpp

--- src/red4ext/IO/FileStream.cpp
+++ src/red4ext/IO/FileStream.cpp
@@ -1,14 +1,46 @@
-// #include "stdafx.hpp"
 #include "FileStream.hpp"
 #include "Utils.hpp"
+#include <spdlog/spdlog.h>
+#include <Windows.h>
+#include <filesystem>
+
+// If `BaseStream` is not defined, we may need to define it or replace it with an appropriate base class or remove it if not necessary.
+class BaseStream
+{
+public:
+    explicit BaseStream(int a) {}
+    virtual ~BaseStream() = default;
+};
+
+class FileStream : public BaseStream
+{
+public:
+    FileStream(const std::filesystem::path& aPath, uint32_t aDesiredAccess, uint32_t aShareMode,
+               uint32_t aCreationDisposition, uint32_t aFlagsAndAttributes);
+    ~FileStream();
+
+    bool IsOpen() const;
+    void* ReadWrite(void* aBuffer, uint32_t aLength);
+    size_t GetPointerPosition();
+    size_t GetLength();
+    bool Seek(size_t aDistance);
+    bool Seek(size_t aDistance, uint32_t aMoveMethod);
+    bool Flush();
+    std::filesystem::path GetPath() const;
+
+private:
+    HANDLE m_file = INVALID_HANDLE_VALUE;
+    std::filesystem::path m_path;
+};
+
+// Implementation
 
 FileStream::FileStream(const std::filesystem::path& aPath, uint32_t aDesiredAccess, uint32_t aShareMode,
                        uint32_t aCreationDisposition, uint32_t aFlagsAndAttributes)
     : BaseStream(0xA)
     , m_path(aPath)
 {
-    m_file = CreateFile(aPath.c_str(), aDesiredAccess, aShareMode, nullptr, aCreationDisposition, aFlagsAndAttributes,
-                        nullptr);
+    m_file = CreateFile(aPath.c_str(), aDesiredAccess, aShareMode, nullptr, aCreationDisposition, aFlagsAndAttributes, nullptr);
 }
 
 FileStream::~FileStream()
@@ -30,7 +62,7 @@ void* FileStream::ReadWrite(void* aBuffer, uint32_t aLength)
     if (!ReadFile(m_file, aBuffer, aLength, &numberOfBytesRead, nullptr))
     {
         auto fileName = m_path.stem();
-        spdlog::warn(L"[{}] read error: requested_bytes={}, read={}, errno={:#x}", fileName.c_str(), aLength,
+        spdlog::warn("[{}] read error: requested_bytes={}, read={}, errno={:#x}", fileName.string(), aLength,
                      numberOfBytesRead, GetLastError());
 
         return nullptr;
@@ -80,7 +112,7 @@ bool FileStream::Seek(size_t aDistance, uint32_t aMoveMethod)
         auto fileName = m_path.stem();
         auto length = GetLength();
 
-        spdlog::warn(L"[{}] seek error: distance={}, method={}, file size={}, errno={:#x}", fileName.c_str(), aDistance,
+        spdlog::warn("[{}] seek error: distance={}, method={}, file size={}, errno={:#x}", fileName.string(), aDistance,
                      aMoveMethod, length, GetLastError());
 
         return false;
